f2648a98dd0860704af2d42a79428cd89ab609bc
Mario Caropreso <mcaropreso@yammer-inc.com>
2013-07-10 11:58:59 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yamoverflow::Application.config.secret_key_base = 'f7669ee4bf25c3aaf8f4a0348290c13aab66b2a6279343ec3694a97746cff117bb9675a4c6653e82c3adcd8175d38afc57a1886fef225e1f583aee004f4b1f2c'
