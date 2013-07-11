b8128aa9c08f1e24c56e8b387a5e054fedbd05cc
Matias Surdi <msurdi@yammer-inc.com>
2013-07-11 13:44:00 +0100

    Added more production settings

--- deployment/build.bash
+++ deployment/build.bash
@@ -13,16 +13,24 @@ puppet apply deployment/init.pp --modulepath=deployment/modules --verbose
 echo "Setting up ruby version"
 ( $RVM list rubies|grep $RUBY_VERSION ) || $RVM install $RUBY_VERSION  
 source $ENVIRONMENT
-gem install bundler
+( gem list|grep bundler ) || gem install bundler
 
 echo "Packaging release $RELEASE..."
 bundle package --all
 
-echo "Copying to $DEPLOY_TARGET"
+echo "Copying to $DEPLOY_TARGET..."
 mkdir -p  $DEPLOY_TARGET
 rsync -a . $DEPLOY_TARGET/
-chown -R $OWNER $DEPLOY_TARGET
 ln -s $ENVIRONMENT $DEPLOY_TARGET/environment
+rm  -f $DEPLOY_TARGET/config/database.yml
+ln -s /etc/yamoverflow/database.yml $DEPLOY_TARGET/config/database.yml
+
+echo "Fixing permissions..."
+chown -R $OWNER $DEPLOY_TARGET
+chmod a+rwX $DEPLOY_TARGET/log
+
+echo "Running migrations..."
+rake db:migrate RAILS_ENV="production" 
 
 echo "Switching version..."
 rm -f $DEPLOY_ROOT/current
