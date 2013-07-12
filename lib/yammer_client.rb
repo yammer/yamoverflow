9861e66040f6e59c506a71294296515c2876d2fc
Mario Caropreso <mcaropreso@yammer-inc.com>
2013-07-12 10:19:33 +0100

    Indentation fixes

--- lib/yammer_client.rb
+++ lib/yammer_client.rb
@@ -6,39 +6,39 @@ class YammerClient
 	end
 
 	def thread_starter(thread_id)
-			message = @yammer_client.get_message(thread_id)
-			message.body[:body][:plain]
+		message = @yammer_client.get_message(thread_id)
+		message.body[:body][:plain]
+	end
+
+	def full_thread(thread_id)
+		messages = @yammer_client.messages_in_thread(thread_id)
+		messages.body
+	end
+
+	def find_tagged_answer(thread_id)
+		messages = @yammer_client.messages_in_thread(thread_id)
+		messages = messages.body[:messages]
+
+		messages = messages.select do |message|
+			message[:thread_id] != message[:id] && message[:body][:plain].match("#yamoverflow")
 		end
 
-		def full_thread(thread_id)
-			messages = @yammer_client.messages_in_thread(thread_id)
-			messages.body
-		end
+		messages.sort_by! {|msg| msg[:id]}
+		answer = messages.last
+		answer[:body][:plain].gsub! "#yamoverflow",""
+		{:id => answer[:id], :body => answer[:body][:plain]}
+	end
 
-		def find_tagged_answer(thread_id)
-			messages = @yammer_client.messages_in_thread(thread_id)
-			messages = messages.body[:messages]
-
-			messages = messages.select do |message|
-				message[:thread_id] != message[:id] && message[:body][:plain].match("#yamoverflow")
+	def topics(thread_id)
+		topics = []
+		thread = @yammer_client.get_thread(thread_id)
+		thread = thread.body
+		thread[:references].each do |reference|
+			if reference[:type] == "topic" && reference[:name] != "Yamoverflow"
+				topics << reference
 			end
-
-			messages.sort_by! {|msg| msg[:id]}
-			answer = messages.last
-			answer[:body][:plain].gsub! "#yamoverflow",""
-			{:id => answer[:id], :body => answer[:body][:plain]}
 		end
 
-		def topics(thread_id)
-			topics = []
-			thread = @yammer_client.get_thread(thread_id)
-			thread = thread.body
-			thread[:references].each do |reference|
-				if reference[:type] == "topic" && reference[:name] != "Yamoverflow"
-					topics << reference
-				end
-			end
-
-			topics
-		end
+		topics
+	end
 end
