b05637dffefcc608ca02a73749b2d60dee22e53e
Mario Caropreso <mcaropreso@yammer-inc.com>
2013-07-12 11:28:18 +0100

    WE HAVE SEARCH

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,9 @@
 class Question < ActiveRecord::Base
 	has_and_belongs_to_many :topics
 	belongs_to :user
+
+	def self.query(q)
+  	q = "%#{q}%"
+		where("title ILIKE ? or answer ILIKE ?",q,q)
+	end
 end
