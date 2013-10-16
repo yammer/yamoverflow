325fd973209e6c6a89300603938c582c515e5b16
Mario Caropreso <mcaropreso@yammer-inc.com>
2013-10-16 10:16:26 +0100

    The env variable YAMMER_NETWORK can be used to limit user sign in based on network

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,9 @@
 # All rights reserved.
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 #
-# THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT. 
+# THIS CODE IS PROVIDED *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT.
 #
-# See the Apache Version 2.0 License for specific language governing permissions and limitations under the License. 
+# See the Apache Version 2.0 License for specific language governing permissions and limitations under the License.
 #
 
 class SessionsController < ApplicationController
@@ -18,11 +18,11 @@ class SessionsController < ApplicationController
     user = User.find_by_auth_token(auth_token)
 
     unless user
-      if auth[:extra][:raw_info][:network_id] != 107
+      if auth[:extra][:raw_info][:network_id] != ENV['YAMMER_NETWORK']
         flash[:error] = "Sorry but access is limited to users of the Microsoft Network"
         redirect_to root_url
       end
-      
+
       user = User.create!(auth_token: auth_token,
                           name: info[:name],
                           permalink: info[:nickname],
